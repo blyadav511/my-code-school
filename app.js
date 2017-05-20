dce32caa1133ac86259cf534416d2bb2ab481c53
Babu Yadav <blyadav511@gmail.com>
2017-05-21 01:05:06 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+
+module.exports = app;
---
+++ run.sh
@@ -0,0 +1,2 @@
+export EMAIL_LIST='["blyadav511@gmail.com"]'
+PORT=8000 node ./bin/www
