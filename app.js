7cb7d69696e2b3787b89df15b2bc33875ca2892b
Babu Yadav <blyadav511@gmail.com>
2017-05-21 12:51:26 +0530

    first temp page

--- app.js
+++ app.js
@@ -13,19 +13,19 @@ app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({
-	extended:false,
-	paramaterLimit:10000,
-	limit: 1024*1024*10
-}));
+// app.use(bodyParser.urlencoded({
+// 	extended:false,
+// 	paramaterLimit:10000,
+// 	limit: 1024*1024*10
+// }));
 
-app.use(bodyParser.json({
-	extended:false,
-	paramaterLimit:10000,
-	limit: 1024*1024*10
-}));
+// app.use(bodyParser.json({
+// 	extended:false,
+// 	paramaterLimit:10000,
+// 	limit: 1024*1024*10
+// }));
 
-app.use(cookieParser());
+// app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
